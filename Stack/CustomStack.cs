404079130ff485ef5302a4a23f1959ffae5f7635
Gabriel Hortmann <gabrielhortmann@gmail.com>
2025-08-15 21:39:40 -0300

    Developed Stack Repo

---
+++ Stack/CustomStack.cs
@@ -0,0 +1,45 @@
+﻿namespace Structures;
+
+class CustomStack<T>
+{
+    private T?[] stack;
+    private int index = 0;
+    private int maxLength = 0;
+
+    public CustomStack(int length)
+    {
+        stack = new T[length];
+        maxLength = length;
+    }
+
+    public void Append(T value)
+    {
+        if (maxLength < index + 1)
+        {
+            Console.WriteLine("Max limit exceeded!");
+            return;
+        }
+
+        stack[index] = value;
+        index++;
+    }
+
+    public void Pop()
+    {
+        if (index - 1 < 0)
+        {
+            Console.WriteLine("Minimum achieved!");
+            return;
+        }
+        index--;
+        stack[index] = default;
+    }
+
+    public void Show()
+    {
+        for (int i = index - 1; i >= 0; i--)
+        {
+            Console.WriteLine(stack[i]);
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Structures;
+
+CustomStack<int> myStack = new CustomStack<int>(10);
+myStack.Append(1);
+myStack.Append(2);
+myStack.Append(3);
+myStack.Append(4);
+myStack.Pop();
+myStack.Pop();
+myStack.Show();
