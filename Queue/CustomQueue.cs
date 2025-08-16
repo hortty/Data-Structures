8e4b4c8edf82ec630b67485b91c60e29227a7a36
Gabriel Hortmann <gabrielhortmann@gmail.com>
2025-08-15 21:39:59 -0300

    Queue data structure

---
+++ Queue/CustomQueue.cs
@@ -0,0 +1,52 @@
+﻿namespace Structures;
+
+class CustomQueue<T>
+{
+    private T?[] items;
+    private int index = 0;
+    private int maxLength = 0;
+
+    public CustomQueue(int length)
+    {
+        items = new T[length];
+        maxLength = length;
+    }
+
+    // could be done using circular buffer
+    public void Add(T value)
+    {
+        if (maxLength < index + 1)
+        {
+            Console.WriteLine("Max limit exceeded!");
+            return;
+        }
+
+        items[index] = value;
+        index++;
+    }
+
+    // could be done using circular buffer
+    public void Remove()
+    {
+        if (index - 1 < 0)
+        {
+            Console.WriteLine("Minimum achieved!");
+            return;
+        }
+
+        for (int i = 0; i < index - 1; i++)
+        {
+            items[i] = items[i + 1];
+        }
+
+        index--;
+    }
+
+    public void Show()
+    {
+        for (int i = 0; i < index; i++)
+        {
+            Console.Write(items[i] + " ");
+        }
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Structures;
+
+CustomQueue<string> myQueue = new CustomQueue<string>(10);
+myQueue.Add("A");
+myQueue.Add("B");
+myQueue.Add("C");
+myQueue.Add("D");
+myQueue.Add("E");
+myQueue.Remove();
+myQueue.Remove();
+myQueue.Show();
