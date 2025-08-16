3f4d006110ce31238811156dc0868f029a8e6929
Gabriel Hortmann <gabrielhortmann@gmail.com>
2025-08-15 21:40:29 -0300

    Coded Deque (double ended) structure

---
+++ Deque (double ended)/CustomDeque.cs
@@ -0,0 +1,82 @@
+﻿namespace Structures;
+
+class CustomDeque<T>
+{
+    private T?[] items;
+    private int index = 0;
+    private int maxLength = 0;
+
+    public CustomDeque(int length)
+    {
+        items = new T[length];
+        maxLength = length;
+    }
+
+    // could be done using circular buffer
+    public void AddFinal(T value)
+    {
+        if (index + 1 >= maxLength)
+        {
+            Console.WriteLine("Max limit exceeded!");
+            return;
+        }
+
+        items[index] = value;
+        index++;
+    }
+
+    public void AddStart(T value)
+    {
+        if (index + 1 >= maxLength)
+        {
+            Console.WriteLine("Max limit exceeded!");
+            return;
+        }
+
+        for (int i = index; i > 0; i--)
+        {
+            items[i] = items[i - 1];
+        }
+
+        items[0] = value;
+        index++;
+    }
+
+    // could be done using circular buffer
+    public void RemoveStart()
+    {
+        if (index - 1 < 0)
+        {
+            Console.WriteLine("Minimum achieved!");
+            return;
+        }
+
+        for (int i = 0; i < index - 1; i++)
+        {
+            items[i] = items[i + 1];
+        }
+
+        index--;
+    }
+
+    public void RemoveEnd()
+    {
+        if (index - 1 < 0)
+        {
+            Console.WriteLine("Minimum achieved!");
+            return;
+        }
+
+        items[index] = default;
+
+        index--;
+    }
+
+    public void Show()
+    {
+        for (int i = 0; i < index; i++)
+        {
+            Console.Write(items[i] + " ");
+        }
+    }
+}
---
+++ Deque (double ended)/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Structures;
+
+CustomDeque<string> myDeque = new CustomDeque<string>(10);
+myDeque.AddFinal("A");
+myDeque.AddFinal("B");
+myDeque.AddFinal("C");
+myDeque.AddStart("A0");
+myDeque.AddFinal("A2");
+myDeque.Show();
+
+Console.WriteLine("\n");
+
+myDeque.RemoveEnd();
+myDeque.RemoveStart();
+myDeque.Show();
